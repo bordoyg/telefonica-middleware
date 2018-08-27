25dfd640efb6c678df22729144a34cb8a02ad872
GBORDOY-AR\gbordoy <gonzalo.bordoy@oracle.com>
2018-08-27 18:04:47 -0300

    -.-

--- src/main/java/com/telefonica/portalmiddleware/controller/BaseController.java
+++ src/main/java/com/telefonica/portalmiddleware/controller/BaseController.java
@@ -1,11 +1,9 @@
 package com.telefonica.portalmiddleware.controller;
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
-@EnableConfigurationProperties
 @EnableScheduling
 public class BaseController {
 
