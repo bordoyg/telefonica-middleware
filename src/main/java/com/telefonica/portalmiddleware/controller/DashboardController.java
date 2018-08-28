5a75c2904258986805d5356524eb5d5832b2ef8a
GBORDOY-AR\gbordoy <gonzalo.bordoy@oracle.com>
2018-08-28 16:51:35 -0300

    dashboard

--- src/main/java/com/telefonica/portalmiddleware/controller/DashboardController.java
+++ src/main/java/com/telefonica/portalmiddleware/controller/DashboardController.java
@@ -1,5 +1,11 @@
 package com.telefonica.portalmiddleware.controller;
 
+import java.util.Properties;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,11 +13,15 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DashboardController extends BaseController{
+	@Autowired
+	@Qualifier("propertyConfigurer")
+	private Properties portalMiddlewareProperties;
+	private final Logger LOG = LogManager.getLogger(getClass());
+	
 	@RequestMapping("/dashboard")
-	public ModelAndView helloWorld() {
-		System.out.println("DashboardController telefonica");
-		String message = "ya no es lo que era, ya no es lo que era...";
-		return new ModelAndView("dashboard", "message", message);
+	public ModelAndView showDashboard() {
+		LOG.debug("showDashboard INICIO");
+		return new ModelAndView("dashboard", "properties", portalMiddlewareProperties);
 	}
 
 }
