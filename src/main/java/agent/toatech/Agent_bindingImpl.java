e805b208fd5d7e1592026b637813e30c729da0e9
GBORDOY-AR\gbordoy <gonzalo.bordoy@oracle.com>
2018-09-27 16:44:35 -0300

    avance

--- src/main/java/agent/toatech/Agent_bindingImpl.java
+++ src/main/java/agent/toatech/Agent_bindingImpl.java
@@ -9,20 +9,39 @@ package agent.toatech;
 
 import java.rmi.RemoteException;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 public class Agent_bindingImpl implements Agent_port_type{
+	private final Logger LOG = LogManager.getLogger(getClass());
     public Message_response_t[] send_message(User_t user, Message_t[] messages) throws RemoteException {
     	Message_response_t[] response=new Message_response_t[1];
-    	Message_response_t respItem=new Message_response_t();
+    	LOG.debug("Mensaje recibido");
+    	for(Message_t msj:messages){
+    		LOG.debug("app_port: " + msj.getApp_port());
+    		LOG.debug("message_id: " + msj.getMessage_id());
+    		LOG.debug("company_id: " + msj.getCompany_id());
+    		LOG.debug("address: " + msj.getAddress());
+    		LOG.debug("send_to: " + msj.getSend_to());
+    		LOG.debug("subject: " + msj.getSubject());
+    		LOG.debug("body: " + msj.getBody());
+    		
+    		Message_response_t respItem=new Message_response_t();
+        	
+        	respItem.setData(msj.getBody());
+        	respItem.setDescription("Descripcion asd");
+        	respItem.setDuration("duration asd");
+        	respItem.setExternal_id("extenrnal id asd");
+        	respItem.setMessage_id(msj.getMessage_id());
+        	respItem.setSent("sent asd");
+        	respItem.setStatus("sent");
+        	
+        	response[0]=respItem;
+    	}
     	
-    	respItem.setData("data asd");
-    	respItem.setDescription("Descripcion asd");
-    	respItem.setDuration("duration asd");
-    	respItem.setExternal_id("extenrnal id asd");
-    	respItem.setMessage_id(123L);
-    	respItem.setSent("sent asd");
-    	respItem.setStatus("sent");
     	
-    	response[0]=respItem;
+    	
+    	
     	
     	return response;
     }
--- src/main/java/com/telefonica/portalmiddleware/controller/DashboardController.java
+++ src/main/java/com/telefonica/portalmiddleware/controller/DashboardController.java
@@ -21,7 +21,7 @@ public class DashboardController extends BaseController{
 	@RequestMapping("/dashboard")
 	public ModelAndView showDashboard() {
 		LOG.debug("showDashboard INICIO log4j");
-		System.out.println("showDashboard INICIO nativo");
+
 		return new ModelAndView("dashboard", "properties", portalMiddlewareProperties);
 	}
 
