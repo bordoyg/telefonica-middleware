9d318308a0adc5fea796467d76cebc0dc4909e66
GBORDOY-AR\gbordoy <gonzalo.bordoy@oracle.com>
2019-01-21 17:52:34 -0300

    se agrega campo al body del mensaje

--- src/main/java/ar/com/telefonica/portalmiddleware/service/inbound/Agent_bindingSkeleton.java
+++ src/main/java/ar/com/telefonica/portalmiddleware/service/inbound/Agent_bindingSkeleton.java
@@ -8,6 +8,7 @@
 package ar.com.telefonica.portalmiddleware.service.inbound;
 
 import java.io.PrintWriter;
+import java.io.StringReader;
 import java.io.StringWriter;
 import java.net.URL;
 import java.rmi.RemoteException;
@@ -16,8 +17,13 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.ResourceBundle;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
 import agent.toatech.Agent_port_type;
 import agent.toatech.Agent_serviceLocator;
@@ -28,21 +34,25 @@ import agent.toatech.User_t;
 public class Agent_bindingSkeleton extends agent.toatech.Agent_bindingSkeleton {
 	private final Logger LOG = LogManager.getLogger(getClass());
 	private static final long serialVersionUID = 1L;
-	
+	private DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();  
+	private DocumentBuilder builder;  
+
 	@Override
     public Message_response_t[] send_message(User_t user,Message_t[] messages) throws RemoteException{
 		int i=0;
 		try{
 	    	LOG.debug("INICIO send_message");
-	    	
+	        builder = factory.newDocumentBuilder();
 			ResourceBundle properties = ResourceBundle.getBundle("portal-middleware");
-			String surl=properties.getString("service.jump.url");
-			LOG.debug("url Jump: " + surl);
-			URL url=new URL(surl);
+			String jumpUrl=properties.getString("service.jump.url");
+			String portalUrl=properties.getString("portal.url");
+			LOG.debug("url Jump: " + jumpUrl);
+			LOG.debug("url portal: " + portalUrl);
+			URL url=new URL(jumpUrl);
 			Agent_serviceLocator serviceFactory=new Agent_serviceLocator();
 			Agent_port_type service=serviceFactory.getagent_interface(url);
 		
-			LOG.debug("Conectando al servicio de JUMP");
+			
 			//cuando en el body se recibe mas de un mensaje el servicio rompe
 			//iterar el array messages[] y hacer una invocacion por cada elemento del array
 			Message_response_t[] responses=new Message_response_t[messages.length];
@@ -50,9 +60,12 @@ public class Agent_bindingSkeleton extends agent.toatech.Agent_bindingSkeleton {
 				Message_response_t respItem=new Message_response_t();
 				Message_t message=messages[i];
 				try{
+					LOG.debug("Modificando mensaje: " + message.getMessage_id());
+					addDataToMessage(message, portalUrl);
 					Message_t[] messgs=new Message_t[1];
 					messgs[0]=message;
 					LOG.debug("Enviando mensaje: " + message.getMessage_id());
+					LOG.debug("Conectando al servicio de JUMP");
 					Message_response_t[] reps= service.send_message(user, messgs);
 					responses[i]=reps[0];
 					LOG.debug("Mensaje enviado a JUMP satisfactoriamente");
@@ -91,4 +104,16 @@ public class Agent_bindingSkeleton extends agent.toatech.Agent_bindingSkeleton {
 	    	return response;
 		}
     }
+
+	private void addDataToMessage(Message_t message, String portalUrl) throws Exception {
+	    Document document = builder.parse(new InputSource(new StringReader(message.getBody())));  
+	    String aid=document.getElementsByTagName("aid").item(0).getTextContent();
+	    String activityUrl=portalUrl + "?activity=" + aid;
+	    LOG.debug("activityUrl= " + activityUrl);
+		StringBuilder sb=new StringBuilder();
+		sb.append(message.getBody());
+		sb=sb.replace(sb.indexOf("<aid>"), sb.indexOf("<aid>") + "<aid>".length(), "<property><label>activityurl</label><value>" + activityUrl + "</value></property><aid>");
+		message.setBody(sb.toString());
+		
+	}
 }
