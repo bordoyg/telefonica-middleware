f45aafcb6b2b8f2e6108d56575e50516d6156c1c
GBORDOY-AR\gbordoy <gonzalo.bordoy@oracle.com>
2019-01-31 17:08:15 -0300

    se agrega un campo

--- src/main/java/agent/toatech/Agent_bindingImpl.java
+++ src/main/java/agent/toatech/Agent_bindingImpl.java
@@ -51,11 +51,12 @@ public class Agent_bindingImpl implements Agent_port_type{
 		try {
 			String contactList=portalMiddlewareProperties.getProperty("repsonsys.contact-list");
 			JSONObject jsonBodyTOA=new JSONObject('{' + msj.getBody() + '}');
-			String jsonRequestMemberRS="{'recordData':{'fieldNames':['EMAIL_ADDRESS_','URL_CONFIRMAR','URL_CANCELAR','URL_MODIFICAR','URL_UBIC_TECNICO', 'MOBILE_NUMBER_','FECHA_CITA', 'FRANJA_HORARIA', 'NOMBRE','NOMBRE_TECNICO'],'records':[['@email@', '@urlPortal@', '@urlPortal@', '@urlPortal@', '@urlPortal@', '@mobileNumber@', '@fechaCita@', '@franjaHoraria@', '@nombre@', '@nombreTecnico@']]},'mergeRule':{'htmlValue':'H','optinValue':'I','textValue':'T','insertOnNoMatch':true,'updateOnMatch':'REPLACE_ALL','matchColumnName1':'EMAIL_ADDRESS_','matchColumnName2':null,'matchOperator':'NONE','optoutValue':'O','rejectRecordIfChannelEmpty':null,'defaultPermissionStatus':'OPTIN'}}";
+			String jsonRequestMemberRS="{'recordData':{'fieldNames':['EMAIL_ADDRESS_','URL_CONFIRMAR','URL_CANCELAR','URL_MODIFICAR','URL_UBIC_TECNICO', 'MOBILE_NUMBER_', 'MOBILE_COUNTRY_', 'FECHA_CITA', 'FRANJA_HORARIA', 'NOMBRE','NOMBRE_TECNICO'],'records':[['@email@', '@urlPortal@', '@urlPortal@', '@urlPortal@', '@urlPortal@', '@mobileNumber@', '@countryCode@', '@fechaCita@', '@franjaHoraria@', '@nombre@', '@nombreTecnico@']]},'mergeRule':{'htmlValue':'H','optinValue':'I','textValue':'T','insertOnNoMatch':true,'updateOnMatch':'REPLACE_ALL','matchColumnName1':'EMAIL_ADDRESS_','matchColumnName2':null,'matchOperator':'NONE','optoutValue':'O','rejectRecordIfChannelEmpty':null,'defaultPermissionStatus':'OPTIN'}}";
 			
 			jsonRequestMemberRS=jsonRequestMemberRS.replaceAll("@email@", jsonBodyTOA.getString("cemail"))
 					.replaceAll("@urlPortal@", createURLPortal(jsonBodyTOA.getInt("aid")))
 					.replaceAll("@mobileNumber@", jsonBodyTOA.getString("ccell"))
+					.replaceAll("@countryCode@", jsonBodyTOA.getString("ccell"))
 					.replaceAll("@fechaCita@", jsonBodyTOA.getString("route_date"))
 					.replaceAll("@franjaHoraria@", jsonBodyTOA.getString("activity_time_slot"))
 					.replaceAll("@nombre@", jsonBodyTOA.getString("activity_customer_name"))
