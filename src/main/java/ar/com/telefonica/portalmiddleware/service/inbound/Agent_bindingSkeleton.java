9747fb1c9f95d788022546939e08e9a59b87aee7
GBORDOY-AR\gbordoy <gonzalo.bordoy@oracle.com>
2019-02-19 17:30:35 -0300

    se cambia respuesta de status de jump: fault x failed

--- src/main/java/ar/com/telefonica/portalmiddleware/service/inbound/Agent_bindingSkeleton.java
+++ src/main/java/ar/com/telefonica/portalmiddleware/service/inbound/Agent_bindingSkeleton.java
@@ -13,7 +13,6 @@ import java.io.StringWriter;
 import java.net.URL;
 import java.rmi.RemoteException;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.ResourceBundle;
 
@@ -39,75 +38,82 @@ public class Agent_bindingSkeleton extends agent.toatech.Agent_bindingSkeleton {
 	private static final long serialVersionUID = 1L;
 	private DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();  
 	private DocumentBuilder builder;  
+	private Agent_port_type service=null;
+	private String portalUrl=null;
+	private static final String FAKE_FAILED_STATUS="fault";
+	
+    public Agent_bindingSkeleton() {
+        super();
+        init();
+    }
 
-	@Override
-    public Message_response_t[] send_message(User_t user,Message_t[] messages) throws RemoteException{
-		int i=0;
-		try{
-	    	LOG.debug("INICIO send_message");
-	        builder = factory.newDocumentBuilder();
+    public Agent_bindingSkeleton(agent.toatech.Agent_port_type impl) {
+        super(impl);
+        init();
+    }
+    private void init(){
+        try {
+			builder = factory.newDocumentBuilder();
 			ResourceBundle properties = ResourceBundle.getBundle("portal-middleware");
 			String jumpUrl=properties.getString("service.jump.url");
-			String portalUrl=properties.getString("portal.url");
+			portalUrl=properties.getString("portal.url");
 			LOG.debug("url Jump: " + jumpUrl);
 			LOG.debug("url portal: " + portalUrl);
 			URL url=new URL(jumpUrl);
 			Agent_serviceLocator serviceFactory=new Agent_serviceLocator();
-			Agent_port_type service=serviceFactory.getagent_interface(url);
-		
-			
-			//cuando en el body se recibe mas de un mensaje el servicio rompe
-			//iterar el array messages[] y hacer una invocacion por cada elemento del array
-			Message_response_t[] responses=new Message_response_t[messages.length];
-			for(i=0; i<messages.length; i++){
-				Message_response_t respItem=new Message_response_t();
-				Message_t message=messages[i];
-				try{
-					LOG.debug("Modificando mensaje: " + message.getMessage_id());
-					addDataToMessage(message, portalUrl);
-					Message_t[] messgs=new Message_t[1];
-					messgs[0]=message;
-					LOG.debug("Enviando mensaje: " + message.getMessage_id());
-					LOG.debug("Conectando al servicio de JUMP");
-					Message_response_t[] reps= service.send_message(user, messgs);
-					responses[i]=reps[0];
-					LOG.debug("Mensaje enviado a JUMP satisfactoriamente");
-				}catch(Exception e){
-					LOG.error("Hubo un error al intentar enviar el mensaje: " + message.getMessage_id(), e);
-					respItem.setStatus(BaseService.STATUS_FAILED);
-					StringWriter sw = new StringWriter();
-					PrintWriter pw = new PrintWriter(sw);
-					e.printStackTrace(pw);
-					respItem.setData(e.getMessage());
-					String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-					respItem.setDescription("Hubo un error: " + timeStamp);
-					respItem.setMessage_id(message.getMessage_id());
-					responses[i]=respItem;
-				}
-			}
-			LOG.debug("FIN send_message");
-			for(i=0; i<responses.length; i++){
-				LOG.debug("Message_response_t[" + (i + 1) + "]" + responses[i]);
-			}
-			return responses;
-		}catch(Exception e){
-			Message_response_t[] response= new Message_response_t[1];
-			Message_response_t respItem=new Message_response_t();
-			respItem.setStatus("fault");
-			StringWriter sw = new StringWriter();
-			PrintWriter pw = new PrintWriter(sw);
-			e.printStackTrace(pw);
-			String sStackTrace = sw.toString(); 
-			respItem.setData(sStackTrace);
-			String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-			respItem.setDescription("Hubo un error: " + timeStamp);
-			
-			LOG.error("Hubo un error al intentar enviar el mensaje: " + timeStamp, e);
-			
-	    	response[i]=respItem;
-	    	LOG.debug("FIN send_message");
-	    	return response;
+			service=serviceFactory.getagent_interface(url);
+		} catch (Exception e) {
+			LOG.error(e);
 		}
+
+    }
+	@Override
+    public Message_response_t[] send_message(User_t user,Message_t[] messages) throws RemoteException{
+		int i=0;
+
+    	LOG.debug("INICIO send_message");
+		//cuando en el body se recibe mas de un mensaje el servicio rompe
+		//iterar el array messages[] y hacer una invocacion por cada elemento del array
+		Message_response_t[] responses=new Message_response_t[messages.length];
+		for(i=0; i<messages.length; i++){
+			Message_response_t respItem=new Message_response_t();
+			Message_t message=messages[i];
+			try{
+				LOG.debug("Modificando mensaje: " + message.getMessage_id());
+				addDataToMessage(message, portalUrl);
+				Message_t[] messgs=new Message_t[1];
+				messgs[0]=message;
+				LOG.debug("Enviando mensaje: " + message.getMessage_id());
+				LOG.debug("Conectando al servicio de JUMP");
+				Message_response_t[] reps= service.send_message(user, messgs);
+				LOG.debug("Mensaje enviado a JUMP, analizando la respuesta");
+				if(FAKE_FAILED_STATUS.equals(reps[0].getStatus())){
+					reps[0].setStatus(BaseService.STATUS_FAILED);
+					LOG.debug("Mensaje no pudo ser enviado por JUMP: " + reps[0]);
+				}
+				if(BaseService.STATUS_SENT.equals(reps[0].getStatus()) 
+						|| BaseService.STATUS_DELIVERED.equals(reps[0].getStatus())){
+					LOG.debug("Mensaje fue procesado por JUMP satisfactoriamente");
+				}
+				responses[i]=reps[0];
+				
+			}catch(Exception e){
+				LOG.error("Hubo un error al intentar enviar el mensaje: " + message.getMessage_id(), e);
+				respItem.setStatus(BaseService.STATUS_FAILED);
+				StringWriter sw = new StringWriter();
+				PrintWriter pw = new PrintWriter(sw);
+				e.printStackTrace(pw);
+				respItem.setData(e.getMessage());
+				String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+				respItem.setDescription("Hubo un error: " + timeStamp);
+				respItem.setMessage_id(message.getMessage_id());
+				responses[i]=respItem;
+			}
+		}
+		LOG.debug("FIN send_message");
+
+		return responses;
+	
     }
 
 	private void addDataToMessage(Message_t message, String portalUrl) throws Exception {
