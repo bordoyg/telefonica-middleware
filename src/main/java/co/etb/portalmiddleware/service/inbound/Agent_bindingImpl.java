3369cb6f8ca0966466a621437668206fdd04a90b
GBORDOY-AR\gbordoy <gonzalo.bordoy@oracle.com>
2019-03-14 13:20:32 -0300

    Se agregan campos body responsys etb

--- src/main/java/co/etb/portalmiddleware/service/inbound/Agent_bindingImpl.java
+++ src/main/java/co/etb/portalmiddleware/service/inbound/Agent_bindingImpl.java
@@ -7,6 +7,16 @@
 
 package co.etb.portalmiddleware.service.inbound;
 
+import org.json.JSONObject;
+
 public class Agent_bindingImpl extends agent.toatech.Agent_bindingImpl{
-	
+	protected String buildBodyRequest(String jsonRequestMemberRS, JSONObject jsonBodyTOA)throws Exception {
+		jsonRequestMemberRS=super.buildBodyRequest(jsonRequestMemberRS, jsonBodyTOA);
+		jsonRequestMemberRS=jsonRequestMemberRS.replaceAll("@appt_confirmed@", jsonBodyTOA.getString("appt_confirmed"))
+				.replaceAll("@appt_rescheduled@", jsonBodyTOA.getString("appt_rescheduled"))
+				.replaceAll("@service_window_start@", jsonBodyTOA.getString("service_window_start"))
+				.replaceAll("@service_window_end@", jsonBodyTOA.getString("service_window_end"));
+		
+		return jsonRequestMemberRS;
+	}
 }
