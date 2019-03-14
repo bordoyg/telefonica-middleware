ce1d82f78da4a00b4c7c7abd3902a02fc0f8309b
GBORDOY-AR\gbordoy <gonzalo.bordoy@oracle.com>
2019-03-14 12:56:00 -0300

    Cambio en diseño para redefinir segun cliente api inbound

---
+++ src/main/java/co/etb/portalmiddleware/service/inbound/Agent_bindingImpl.java
@@ -0,0 +1,12 @@
+/**
+ * Agent_bindingImpl.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package co.etb.portalmiddleware.service.inbound;
+
+public class Agent_bindingImpl extends agent.toatech.Agent_bindingImpl{
+	
+}
---
+++ src/main/java/co/etb/portalmiddleware/service/inbound/Agent_bindingSkeleton.java
@@ -0,0 +1,20 @@
+/**
+ * Agent_bindingSkeleton.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package co.etb.portalmiddleware.service.inbound;
+
+public class Agent_bindingSkeleton extends agent.toatech.Agent_bindingSkeleton {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Agent_bindingSkeleton() {
+        this.impl = new Agent_bindingImpl();
+    }
+}
